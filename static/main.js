c4077a5b6578b3dec2a6b9cdb09904324e292675
Kameron <kpierre3@buffalo.edu>
2024-03-24 17:11:15 -0400

    Arrow up and down

--- static/main.js
+++ static/main.js
@@ -1,39 +1,41 @@
-/*  update the value of a tag in the html file
-    document.getElementById('name_of_specific_id').innerHTML = "Message you want it to be changed to" 
-*/
-
-/*  way to call this file in the html file   
-    <script type="text/javascript" src="{{url_for('static', filename='main.js')}}"></script>
- */
-
-// moved login and register into static because they couldn't be found in templates with current setup
-
 function update_posts(post_message) { 
-    console.log("printing a test print");
-
-    if (post_message.length != 0) {
-        console.log(post_message);
-        console.log(post_message[postMessage.length - 1]);
-        console.log(post_message[postMessage.length - 1]["body"]);
-        console.log(post_message[postMessage.length - 1]["subject"])
-
-        const message_body = document.getElementById("test_cat");
-        const message_subject = document.getElementById("subject1");
+    const message_id = document.getElementById("post_id");
+    const message_subject = document.getElementById("subject1");
+    const message_body = document.getElementById("body_placeholder");
     
-        message_body.innerHTML += post_message[postMessage.length - 1]["body"];
-        message_subject.innerHTML = post_message[postMessage.length - 1]["subject"];
-    }
+    message_id.innerHTML = post_message[post_message.length -1]["ID"];
+    message_subject.innerHTML = post_message[post_message.length - 1]["subject"];
+    message_body.innerHTML = post_message[post_message.length - 1]["creator"] + ": " + post_message[post_message.length - 1]["body"];
 }
 
+
 function startup() { 
-    /* const explore = document.getElementsByClassName("explore-page");
-    explore.innerHTML = "EXPLORE"; */
+    const explore = document.getElementById("modifybyJS");
+    explore.innerHTML = "EXPLORE"; 
 
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
+        if (this.readyState === 4 && this.status === 200) { 
             const message = JSON.parse(this.response);
-            update_posts(message);
+            if (message.length != 0) { update_posts(message); }
+        }
+    }
+    
+    request.open("GET", "/startup");
+    request.send();
+}
+document.get
+
+function arrow_up() {
+    const request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) { 
+            const message = JSON.parse(this.response);
+            if (message.length != 0) { 
+                post_id = Number(document.getElementById("post_id").innerHTML);
+                if (post_id != -1 && post_id < message.length - 1) { update_posts(Array(message[post_id + 1])); }
+             }
         }
     }
     
@@ -41,3 +43,20 @@ function startup() {
     request.send();
 }
 
+
+function arrow_down() {
+    const request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) { 
+            const message = JSON.parse(this.response);
+            if (message.length != 0) {
+                post_id = Number(document.getElementById("post_id").innerHTML);
+                if (post_id != -1 && post_id > 0) { update_posts(Array(message[post_id - 1])); }
+             }
+        }
+    }
+    
+    request.open("GET", "/startup");
+    request.send();
+}
