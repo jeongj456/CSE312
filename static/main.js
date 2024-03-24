7044fe0cd47080d30ec75d92262d59a40478ccbd
Kameron <kpierre3@buffalo.edu>
2024-03-24 14:36:46 -0400

    js changes to show posts

--- static/main.js
+++ static/main.js
@@ -8,14 +8,36 @@
 
 // moved login and register into static because they couldn't be found in templates with current setup
 
+function update_posts(post_message) { 
+    console.log("printing a test print");
+
+    if (post_message.length != 0) {
+        console.log(post_message);
+        console.log(post_message[postMessage.length - 1]);
+        console.log(post_message[postMessage.length - 1]["body"]);
+        console.log(post_message[postMessage.length - 1]["subject"])
+
+        const message_body = document.getElementById("test_cat");
+        const message_subject = document.getElementById("subject1");
+    
+        message_body.innerHTML += post_message[postMessage.length - 1]["body"];
+        message_subject.innerHTML = post_message[postMessage.length - 1]["subject"];
+    }
+}
 
 function startup() { 
+    /* const explore = document.getElementsByClassName("explore-page");
+    explore.innerHTML = "EXPLORE"; */
+
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            console.log(this.response);
+            const message = JSON.parse(this.response);
+            update_posts(message);
         }
     }
-    request.open("GET", "/startup")
+    
+    request.open("GET", "/startup");
+    request.send();
 }
 
