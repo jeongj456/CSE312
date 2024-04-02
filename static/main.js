e3731d2653b9c80dcde4a9d041a7133efecfe1cc
Kameron <kpierre3@buffalo.edu>
2024-04-02 17:48:57 -0400

    FInished cleaning up js and backend

--- static/main.js
+++ static/main.js
@@ -1,32 +1,42 @@
 function update_posts(post_message) { 
-    const message_id = document.getElementById("post_id");
+
+    // change post_id to be the id of the post_message
+    const post_id = document.getElementById("post_id");
+    post_id.setAttribute("value", post_message[post_message.length - 1]["ID"]);
+
+    // change subject1 to be the subject of post_message
     const message_subject = document.getElementById("subject1");
-    const message_body = document.getElementById("body_placeholder");
-    
-    document.getElementById("post_id").setAttribute("value", post_message[post_message.length - 1]["ID"]);
     message_subject.innerHTML = post_message[post_message.length - 1]["subject"];
+
+    // change body_placeholder to be the body of post_message
+    const message_body = document.getElementById("body_placeholder");
     message_body.innerHTML = post_message[post_message.length - 1]["creator"] + ": " + post_message[post_message.length - 1]["body"];
 }
 
 
 function startup() { 
+
+    // Modify webpage with js on refresh
     const explore = document.getElementById("modifybyJS");
     explore.innerHTML = "EXPLORE"; 
 
+    // Create request and wait to recieve response
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) { 
 
+            // Dictionary of message, containing subject, body, and post_id
             const message = JSON.parse(this.response);
-            if (message.length != 0) { 
+            const post_id = Number(document.getElementById("post_id")["value"])
 
-                if (Number(document.getElementById("post_id")["value"]) == -1) { update_posts(message); }
-                else { update_posts(Array(message[Number(document.getElementById("post_id")["value"])])); }
-                getcomments();
-            }
+            // TODO figure out what first part does and displpay message details
+            if (post_id == -1) { update_posts(message); }
+            else { update_posts(Array(message[post_id])); }
+            getcomments();
         }
     }
     
+    // open and send request to route /startup
     request.open("GET", "/startup");
     request.send();   
 }
@@ -42,7 +52,7 @@ function arrow_control(arrow_direction) {
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) { 
 
-            // TODO write what message represents
+            // Dictionary of message, containing subject, body, and post_id
             const message = JSON.parse(this.response);
 
             // Get the post id, check a message exists, and that there is a above/below it based on arrow direction
@@ -50,7 +60,7 @@ function arrow_control(arrow_direction) {
             if ((arrow_direction == -1 && post_id != -1 && post_id > 0) || 
             (arrow_direction == 1 && post_id != -1 && message.length != 0 && post_id < message.length - 1)) { 
 
-                // TODO comment, increment/decrement post_id, and displpay the new message details
+                // update post subject, body, and id to message. Then increment/decrement post_id, and displpay new message details
                 update_posts(Array(message[post_id + arrow_direction])); 
                 document.getElementById("post_id").setAttribute("value", post_id + arrow_direction); 
                 getcomments();
@@ -63,6 +73,7 @@ function arrow_control(arrow_direction) {
     request.send();
 }
 
+
 function getcomments() {
 
     // Create request and wait to recieve response
@@ -70,19 +81,16 @@ function getcomments() {
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
 
-            // TODO figure out what message is and check it is not empty
+            // Dictionary of message containing owner and body
             const message = JSON.parse(this.response);
-            console.log(message);
-            if (message.length != 0) {
 
-                // Clear comment box
-                const comment_box = document.getElementById("comment_box");
-                comment_box.innerHTML = "";
+            // Clear comment box
+            const comment_box = document.getElementById("comment_box");
+            comment_box.innerHTML = "";
 
-                // Write all messages specific to post in comment box
-                for (let i = 0; i < message.length; i++) { 
-                    comment_box.innerHTML += message[i]["postowner"] + ": " + message[i]["body"] + "<br>";
-                }
+            // Write all messages specific to post in comment box
+            for (let i = 0; i < message.length; i++) { 
+                comment_box.innerHTML += message[i]["postowner"] + ": " + message[i]["body"] + "<br>";
             }
 
         }
@@ -90,23 +98,5 @@ function getcomments() {
 
     // open and send the request with the specific post_id included in the route
     request.open("GET", "/getcomments/" + Number(document.getElementById("post_id")["value"]));
-    request.send();
-}
-
-
-function modify_local() {
-
-    // Create request and wait to recieve response
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) { 
-
-            // TODO figure out what this represents
-            Number(document.getElementById("post_id")["value"]) = JSON.parse(this.response);
-        }
-    }
-    
-    // open and send request to route modify_local
-    request.open("GET", "/modify_local");
     request.send();
 }
