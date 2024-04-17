851b00f6a553198666cbc680cac58f237dd4b4ea
mdtornab <mdtornab@buffalo.edu>
2024-04-17 16:39:16 -0400

    Comments are not being appropriately retrieved for websockets

--- app.py
+++ app.py
@@ -304,6 +304,7 @@ def maxPostID(data):
         placement= post
         if direction == -1 and post > 0 or direction == 1 and ID != 0 and post < ID-1:
             placement += direction
+        # For some reason this line appears to be retrieving nothing when stuff exists.
         comments = comments_collection.find({"POSTID":placement},{"_id":0})
         emit('get max', {"postID": placement, 'comments':list(comments)}, broadcast=False)
 
